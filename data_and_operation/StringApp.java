856bf3163819935a9a2be23a9259651b510d450c
Yoo-Su-Hyeon <jjongari01@naver.com>
2026-07-20 19:39:05 +0900

    data_and_operation

---
+++ data_and_operation/StringApp.java
@@ -0,0 +1,21 @@
+public class StringApp {
+
+    public static void main(String[] args) {
+        //Character VS String 
+        // 작은 따옴표 : 문자 : Character
+        // 큰 따옴표 : 문자열 : String
+        System.out.println("Hello World"); // String
+        System.out.println('H'); // Character
+        System.out.println("H"); 
+
+        System.out.println("Hello " 
+        + "World");
+
+        // new line : \n
+        System.out.println("Hello \nWorld");
+
+        // escape : \" >> 큰 따옴표를 일반 문자열로 출력
+        System.out.println("Hello \"World\"");// Hello "World"
+    }
+
+}
